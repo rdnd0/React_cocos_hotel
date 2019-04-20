6317f45348c2f2d70a05b4a8bdf7d72ce1118934
David Redondo <rdnd201@gmail.com>
2019-04-20 11:53:52 +0200

    Updates

---
+++ precache-manifest.9b235f2825fb414f9046d572c9da0194.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13dd12ea320e53a67550",
+    "url": "/React_cocos_hotel/static/js/main.8696a555.chunk.js"
+  },
+  {
+    "revision": "ff4aa477406a0543912e",
+    "url": "/React_cocos_hotel/static/js/runtime~main.7fbaff74.js"
+  },
+  {
+    "revision": "69c7844630a5d9625448",
+    "url": "/React_cocos_hotel/static/js/2.85feb376.chunk.js"
+  },
+  {
+    "revision": "12a82d104d6099fb21f3c2f341328149",
+    "url": "/React_cocos_hotel/index.html"
+  }
+];
